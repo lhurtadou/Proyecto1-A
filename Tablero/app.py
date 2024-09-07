ffc3d3aa6360af0b53a476b20e971af745c286f2
luna-as18 <85699323+luna-as18@users.noreply.github.com>
2024-09-06 20:37:16 -0500

    path

--- Tablero/app.py
+++ Tablero/app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import pandas as pd
 
 # Lee el archivo CSV
-file_path = './Enunciado/DatosPS.csv'
+file_path = '../Enunciado/DatosPS.csv'
 df = pd.read_csv(file_path)
 
 custom_order = ['diciembre', 'enero', 'febrero', 'marzo', 'abril', 'mayo', 
